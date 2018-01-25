974965199195aae3e2f707411aba9601bfd8dfd0
Shohel Kowser <shohelkowser@gmail.com>
2018-01-26 00:04:21 +0600

    chenge backgorund color

--- header.php
+++ header.php
@@ -16,10 +16,11 @@
 
 	<link rel="stylesheet" type="text/css" href="css/set1.css" />
 	<!-- enno -->
-	<link href="css/style.css" rel="stylesheet">
+	
 	<!-- product -->
 	<link href="css/product.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
+
 	
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -35,6 +36,7 @@
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet">
 
 </head>
 	<body>
